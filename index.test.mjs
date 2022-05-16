4e89e5e0d39f2884740da516a2a6e0facfd3defc
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-05-16 15:22:32 -0400

    Use Prettier to normalize tests

--- index.test.mjs
+++ index.test.mjs
@@ -1,25 +1,31 @@
-import postcss from "postcss";
 import { readFileSync } from "node:fs";
 import path from "node:path";
+import postcss from "postcss";
+import prettier from "prettier";
 import { describe, it, expect } from "vitest";
 
 const plugin = (await import(".")).default;
 
-async function run(input, output, opts = {}, postcssOpts = {}) {
+// We don't care about formatting differences, so normalize with prettier
+function format(css) {
+  return prettier.format(css, { parser: "css" });
+}
+
+async function run(input, output, opts, postcssOpts = {}) {
   let result = await postcss([plugin(opts)]).process(input, postcssOpts);
-  expect(result.css).toEqual(output);
+  expect(format(result.css)).toEqual(format(output));
   expect(result.warnings()).toHaveLength(0);
 }
 
 describe("postcss-assign-layer", () => {
-  it("does the thing", async () => {
+  it("adds component layer to module.css files by default", async () => {
     const filePath = path.resolve("test/fixtures/component.module.css");
     const file = readFileSync(filePath, "utf-8");
 
     await run(
       file,
-      "@layer components {a {\n  color: BurlyWood;\n}\n}\n",
-      {},
+      "@layer components { a { color: BurlyWood; } }",
+      undefined,
       {
         from: filePath,
       }
