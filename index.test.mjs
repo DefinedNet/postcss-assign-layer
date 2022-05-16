3c1de87e8aceeaec8d230dab78d27e9da187d52a
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-05-16 15:34:31 -0400

    Expand test cases

--- index.test.mjs
+++ index.test.mjs
@@ -18,14 +18,97 @@ async function run(input, output, opts, postcssOpts = {}) {
 }
 
 describe("postcss-assign-layer", () => {
-  it("adds component layer to module.css files by default", async () => {
+  it("adds component layer to entire *.module.css file by default", async () => {
     const filePath = path.resolve("test/fixtures/component.module.css");
     const file = readFileSync(filePath, "utf-8");
-
     await run(
       file,
-      "@layer components { a { color: BurlyWood; } }",
+      `@layer components {
+        a {
+          color: BurlyWood;
+        }
+
+        i {
+          color: WhiteSmoke;
+        }
+      }`,
       undefined,
+      {
+        from: filePath,
+      }
+    );
+  });
+
+  it("allows customizing the layer name layer to module.css files by default", async () => {
+    const filePath = path.resolve("test/fixtures/component.module.css");
+    const file = readFileSync(filePath, "utf-8");
+    await run(
+      file,
+      `@layer custom {
+        a {
+          color: BurlyWood;
+        }
+
+        i {
+          color: WhiteSmoke;
+        }
+      }`,
+      { layerName: "custom" },
+      {
+        from: filePath,
+      }
+    );
+  });
+
+  it("does not apply to non-module css files by default", async () => {
+    const filePath = path.resolve("test/fixtures/base.css");
+    const file = readFileSync(filePath, "utf-8");
+    await run(
+      file,
+      `a {
+        color: FireBrick;
+      }`,
+      undefined,
+      {
+        from: filePath,
+      }
+    );
+  });
+
+  it("allows specifying files with include pattern", async () => {
+    const filePath = path.resolve("test/fixtures/base.css");
+    const file = readFileSync(filePath, "utf-8");
+    await run(
+      file,
+      `@layer components {
+        a {
+          color: FireBrick;
+        }
+      }`,
+      {
+        include: "**/base.css",
+      },
+      {
+        from: filePath,
+      }
+    );
+  });
+
+  it("does not process module file if other include pattern is provided", async () => {
+    const filePath = path.resolve("test/fixtures/component.module.css");
+    const file = readFileSync(filePath, "utf-8");
+    await run(
+      file,
+      `a {
+        color: BurlyWood;
+      }
+
+      i {
+        color: WhiteSmoke;
+      }`,
+      {
+        include: "**/base.css",
+      },
       {
         from: filePath,
       }
