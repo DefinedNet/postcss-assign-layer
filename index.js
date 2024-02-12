fb18f7f91478779367d2dd1719449145e79540bd
Ian VanSchooten <ian.vanschooten@gmail.com>
2024-02-12 09:18:13 -0500

    Turn rule config into array

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 const { createFilter } = require("@rollup/pluginutils");
 
+const DEFAULT_INCLUDE = "**/*.module.css";
+const DEFAULT_LAYERNAME = "components";
+
 /**
  * @type {import('postcss').PluginCreator}
  */
-module.exports = ({
-  include = "**/*.module.css",
-  layerName = "components",
-} = {}) => {
-  const filter = createFilter(include);
+module.exports = (
+  configItems = [
+    {
+      include: DEFAULT_INCLUDE,
+      layerName: DEFAULT_LAYERNAME,
+    },
+  ]
+) => {
+  const filters = [];
+
+  for (const config of configItems) {
+    const filter = createFilter(config.include ?? DEFAULT_INCLUDE);
+    filters.push({ filter, layerName: config.layerName ?? DEFAULT_LAYERNAME });
+  }
 
   return {
     postcssPlugin: "postcss-assign-layers",
     async Once(root, { AtRule }) {
       const inputFile = root.source.input.file;
-      if (!filter(inputFile)) return;
+      const layerNames = [];
 
-      const layer = new AtRule({
-        name: "layer",
-        params: layerName,
-        nodes: root.nodes,
-      });
-      root.removeAll();
-      root.append(layer);
+      for (const { filter, layerName } of filters) {
+        if (filter(inputFile)) {
+          layerNames.push(layerName);
+        }
+      }
+
+      for (const layerName of layerNames) {
+        const layer = new AtRule({
+          name: "layer",
+          params: layerName,
+          nodes: root.nodes,
+        });
+        root.removeAll();
+        root.append(layer);
+      }
     },
   };
 };
